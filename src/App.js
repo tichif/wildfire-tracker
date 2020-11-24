527cceefc3b57f853029a15e8a7d4713ed59471b
Dalzon Charles-Hébert <charlby5@gmail.com>
2020-11-23 21:52:06 -0600

    Show the map and the location marker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Map from './components/Map'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello world</h1>
+    <div>
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.js
@@ -0,0 +1,26 @@
+import GoogleMapReact from 'google-map-react';
+import LocationMarker from './LocationMarker';
+
+const Map = ({ center, zoom }) => {
+  return (
+    <div className='map'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API_KEY }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <LocationMarker lat={center.lat} lng={center.lng} />
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756,
+  },
+  zoom: 6,
+};
+
+export default Map;
